e166df84db77e14a0b66c1d00b7e76d81c05bbd2
soheima <soheima@sociossports.nl>
2020-03-28 18:13:53 +0100

    finalised first animation

--- js/index.js
+++ js/index.js
@@ -11,44 +11,68 @@ function setup(){
 
     graphic = createGraphics(windowWidth, windowHeight);
 
+    graphic.fill('#DBF685')
     graphic.textSize(500);
     graphic.textAlign(CENTER,CENTER);
     graphic.text("spring", windowWidth*0.5,windowHeight *0.5 - 100)
+    
 
 }
 
 function draw() {
+
+  //
     background("#E84CA6")
+
+   
     
-    const tiles = 10
-    const tileSize = 1600 / tiles
+    const tiles = 10;
+    const tileSize = windowWidth / tiles
     
     // loop over each tile
-    for (let x = 0; x < tiles; x = x + 1) {
-      for (let y = 0; y < tiles; y = y + 1) {
+    for (let x = 0; x < tiles; x++) {
+      //the first for loop runs 10 times
+    
+      for (let y = 0; y < tiles; y++)
+       {
+       
+        // our second for loop runs 10 times for every loop x runs 
+        // y * 10
 
-        const distortion = sin(frameCount * 0.05 + x * 0.5 + y * 0.3) * 50
+        // the speed of the wave frameCount * 0.05
+        // the total equation is the distance of the wave 
+        //x * 0.5 this controls the speed of the x axis - note the smaller the we times the less difference
+        //there is between frames
+        const distortion = sin(frameCount * 0.05 + x * 0.5 + y * 0.5) * 70
      
         // think of this as applying the grid to the source in the graphics buffer
-        const sx = x * tileSize + distortion
+        //sx where we start X
+        const sx = x * tileSize 
+        //sy wwhere we start Y 
         const sy = y * tileSize
-        const sw = tileSize
-        const sh = tileSize
+        //how wide is this
+        const sw = tileSize 
+        //the height of the tile
+        const sh = tileSize + distortion
   
         // and this as applying the grid to the destination on the canvas
+        //where it lands on the x
         const dx = x * tileSize
+        //where we it lands on the Y
         const dy = y * tileSize
+        //the width when it animates
         const dw = tileSize
-        const dh = tileSize
+        //height when it animates 
+        const dh = tileSize 
   
         //	grided image from buffer into main canvas
         image(graphic, dx, dy, dw, dh, sx, sy, sw, sh)
-      }
+     }
     }
   }
   
   
   
-// function windowResized() {
-//     resizeCanvas(windowWidth, windowHeight);
-//   }
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+  }
